89b2fa172794a46308185ae6718468d4058186ab
Daniil <136441101+tohiro15@users.noreply.github.com>
2025-03-13 23:39:24 +0800

    chore: настройка проекта и создание базовой структуры
    - Настроена основная структура проекта
    - Добавлены базовые каталоги и файлы
    - Подготовлена среда для дальнейшей разработки

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bootstrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Project initialized!");
     }
 }
